15e8a2b698fbdab6a053afd4a8c5fe0c7aafda97
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-04-28 21:36:11 +0600

    alumni test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -719,13 +719,69 @@
         <div class="row">
             <div class="col-lg-9 col-md-8 col-sm-6">
                 <div class="section-title">
-                    <span>Our Team</span>
-                    <h2>Meet our team</h2>
+                    <span>Our Alumni</span>
+                    <h2>Meet our Alumni</h2>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6">
                 <div class="team__btn">
                     <a href="#" class="primary-btn normal-btn">View All</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="team__item set-bg" data-setbg="/img/alumniImage/1_rishad_shafiq.jpg">
+                    <div class="team__text">
+                        <div class="team__title">
+                            <h5>Rishad Shafik</h5>
+                            <span>Ex IUTDS</span>
+                        </div>
+                        <p>Vestibulum dapibus odio quam, sit amet hendrerit dui ultricies consectetur. Ut viverra
+                            porta leo, non tincidunt mauris condimentum eget. Vivamus non turpis elit. Aenean
+                            ultricies nisl sit amet.</p>
+                        <div class="team__social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="team__item set-bg" data-setbg="/img/alumniImage/2_alvee_akand.jpg">
+                    <div class="team__text">
+                        <div class="team__title">
+                            <h5>Alvee Akand</h5>
+                            <span>Ex President IUT-15</span>
+                        </div>
+                        <p>Vestibulum dapibus odio quam, sit amet hendrerit dui ultricies consectetur. Ut viverra
+                            porta leo, non tincidunt mauris condimentum eget. Vivamus non turpis elit. Aenean
+                            ultricies nisl sit amet.</p>
+                        <div class="team__social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="team__item set-bg" data-setbg="/img/alumniImage/3_wakil_ahmed_arnob.jpg">
+                    <div class="team__text">
+                        <div class="team__title">
+                            <h5>Wakil Ahmed Arnab</h5>
+                            <span>Ex Vice President IUT-16</span>
+                        </div>
+                        <p>Vestibulum dapibus odio quam, sit amet hendrerit dui ultricies consectetur. Ut viverra
+                            porta leo, non tincidunt mauris condimentum eget. Vivamus non turpis elit. Aenean
+                            ultricies nisl sit amet.</p>
+                        <div class="team__social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
